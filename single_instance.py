0f5a10c8813efbb7c6d28d4abb6109e03fe96f40
huarkiou <huarkiou@outlook.com>
2026-05-16 22:25:35 +0800

    feat: add single-instance mutex check

---
+++ single_instance.py
@@ -0,0 +1,17 @@
+"""Single-instance enforcement via Windows named mutex."""
+import sys
+import ctypes
+
+
+MUTEX_NAME = "Global\\AIRobotUI_SingleInstance"
+
+
+def ensure_single_instance() -> bool:
+    """Try to acquire named mutex. Returns True if we're the only instance."""
+    if sys.platform != "win32":
+        return True
+    kernel32 = ctypes.windll.kernel32
+    handle = kernel32.CreateMutexW(None, True, MUTEX_NAME)
+    if kernel32.GetLastError() == 183:  # ERROR_ALREADY_EXISTS
+        return False
+    return True
